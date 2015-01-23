8977f7be21b513ebf67be2333bd2e9fa8fe5004a
Kelvin Munzwa <tkmunzwa@gmail.com>
2015-01-23 19:15:01 +0200

    Added console.capture, console.release, console.dump methods

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 ##Save JS console output to JSON file
 
-Add console.save to your console object.  console.save(object, filename) will create a file of the object you save, and immediately download it to the filename you specify.  I find this useful for a number of things.
+Adds utility methods to capture (and save) console output to a file.
 
 Example:
 ```JavaScript
-var data = { 
-  hey: "yo",
-  key: "value"
-};
+console.capture() //start capturing console log/warn/error to buffer
+console.log("log1"); //saved to buffer, nothing echoed in browser console
+console.error('Level 5 error'); //also saved to buffer
+console.dump('mymessages.json'); //downloads file containing log buffer since console.capture() call
 
-// this automatically downloads/formats the "data" object to "whatever.json"
-console.save(data, 'whatever.json'); 
-```
+console.release() //stop capturing console output and clear buffer
--- console-save.js
+++ console-save.js
@@ -1,27 +1,53 @@
+//console.save from http://bgrins.github.io/devtools-snippets/#console-save
 (function(console){
 
-console.save = function(data, filename){
+    console.save = function(data, filename){
 
-    if(!data) {
-        console.error('Console.save: No data')
-        return;
+        if(!data) {
+            console.error('Console.save: No data');
+            return;
+        }
+
+        if(!filename) filename = 'console.json';
+
+        if(typeof data === "object"){
+            data = JSON.stringify(data, undefined, 4);
+        }
+
+        var blob = new Blob([data], {type: 'text/json'}),
+            e    = document.createEvent('MouseEvents'),
+            a    = document.createElement('a');
+
+        a.download = filename
+        a.href = window.URL.createObjectURL(blob);
+        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':');
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        a.dispatchEvent(e);
     }
 
-    if(!filename) filename = 'console.json'
+    var og_log, og_warn, og_error, buffer = [];
+    console.capture = function() {
+        og_log = console.log;
+        og_warn = console.warn;
+        og_error = console.error;
 
-    if(typeof data === "object"){
-        data = JSON.stringify(data, undefined, 4);
-    }
+        console.log = function () {
+            buffer[buffer.length] = arguments[0];
+            og_log('Trapping console output to buffer. call console.dump() to save, console.release() to stop');
+            return 'Trapping console output to buffer. call console.dump() to save, console.release() to stop';
+        }.bind(this);
+        console.warn = console.log;
+        console.error = console.log;
+    };
 
-    var blob = new Blob([data], {type: 'text/json'}),
-        e    = document.createEvent('MouseEvents'),
-        a    = document.createElement('a');
+    console.dump = function(filename){
+        console.save(buffer, filename);
+    };
 
-    a.download = filename;
-    a.href = window.URL.createObjectURL(blob);
-    a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':');
-    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    a.dispatchEvent(e);
- };
-
+    console.release = function(){
+        buffer = [];
+        console.log = og_log;
+        console.warn = og_warn;
+        console.err = og_error;
+    };
 })(console);
